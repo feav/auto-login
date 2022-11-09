d6c171e44e95f57f93b751be8d7ffd79e001c5a6
armel embolo <feavfeav@gmail.com>
2022-11-09 14:47:36 +0100

    epuration du code et verification si admin

--- auto-login.php
+++ auto-login.php
@@ -11,45 +11,14 @@
 	Author URI: http://www.global-solutions-group.com
 */
 
-
-header("Access-Control-Allow-Origin: *");
-
-define("WPAUTOLOG_PLUGIN_FILE",__FILE__);
-
-define("WPAUTOLOG_DIR", plugin_dir_path(__FILE__));
-	 
-define("WPAUTOLOG_URL", plugin_dir_url(__FILE__));
-
-define("WPAUTOLOGT_API_URL_SITE", get_site_url() . "/");
-
-define("WPAUTOLOG_POST_TYPE", "wp_auto_login");
-
-define("WPPRODUCT_POST_TYPE", "product");
-
-if(isset($_GET['debug'])){
-    ini_set("display_errors", 1);
-    ini_set("display_startup_errors", 1);
-    error_reporting(E_ALL);
-}
-
 class WPAUTOLOG {
-    private $percent = 10;
-    protected $post_type = WPAUTOLOG_POST_TYPE;
+    protected $post_type = "";
+    protected $site_url = "";
     function __construct() {
-        if (! wp_next_scheduled ( '_all_refresh_daily_event' )) {
-            wp_schedule_event(time(), 'daily',  '_all_refresh_daily_event' );
-        }
-
-        add_action('_all_refresh_daily_event', array(&$this,  'refresh_all_car') );
-
-        add_action( 'rest_api_init',  array(&$this, 'register_endpoints') );
-        
+        $this->post_type = "wp_auto_login";
+        $this->site_url =  get_site_url();
         add_filter('wp_authenticate_user',  array(&$this,'my_auth_login'),10,2);
-        
-
-        add_action('init', array(__CLASS__, 'enquee_scripts'));
         $this->init_ajax_api();
-
     }
     function init_ajax_api(){
         add_action( "wp_ajax_".$this->post_type, array(&$this,"ajax_callback") );
@@ -71,26 +40,7 @@ class WPAUTOLOG {
     function ajax_callback(){
         $user_id = get_current_user_id();
         $module = $this->get_var("function");
-        if($module == 'getuser'){
-            if($user_id){
-                global $current_user;
-                get_currentuserinfo();
-                $email =  $current_user->user_email;
-                $pwd = get_user_meta($user_id,"pwd",true);
-                $data = array(
-                    "response"=>200,
-                    "data"=> array(
-                        "email"=>$email,
-                        "pwd"=>$pwd,
-                        "site"=>$current_user->user_url. "/wp-admin/admin-ajax.php"
-                    )
-                );
-                echo json_encode($data);
-            }else{
-                echo json_encode(array("response"=>400));
-            }
-            
-        }else if($module == 'login'){
+        if($module == 'login'){
             $data = array();
             $username = $data['user_login'] = $this->get_var("username");
             $data['user_password'] =  $this->get_var("password");
@@ -111,12 +61,6 @@ class WPAUTOLOG {
         }
         die();
     }
-
-    public static function enquee_scripts(){
-        // wp_register_script( 'autologin-js', WPAUTOLOG_URL . "/autologin.js" ,'','1.0', true);
-        // wp_enqueue_script( 'autologin-js' );
-    }
-
     function my_auth_login ($user, $password) {
         if ( $user ){
             $url = $user->user_url . '/wp-admin/admin-ajax.php?';
@@ -140,13 +84,6 @@ class WPAUTOLOG {
             return $username;
         }
    }
-
-    function refresh_all_car() {
-    }
-
- 
-    function refresh_one_car() {
-    }
   
 }
 $global_wp_auto_car = new WPAUTOLOG();
